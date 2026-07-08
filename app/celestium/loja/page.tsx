16d75b75c670ca4f19c02401ce82d0dd84757bd5
andriele <rafael.k@aluno.senai.br>
2026-07-08 15:19:21 -0300

    s

--- app/celestium/loja/page.tsx
+++ app/celestium/loja/page.tsx
@@ -1,5 +1,4 @@
 import FooterLojaCeslestium from "./FooterLoja";
-import HeaderLojaCelestium from "./Header";
 import NavBarLoja from "./NavbarLoja";
 import TabelaDeVendas from "./TabelaDeVendas";
 
@@ -8,7 +7,6 @@ export default function LojaCelestium () {
     return (
         <div className="flex flex-col items-center justify-center font-sans">
             <NavBarLoja />
-            <HeaderLojaCelestium />
             <TabelaDeVendas />
             <FooterLojaCeslestium />
         </div>
